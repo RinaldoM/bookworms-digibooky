5d4eda80f70bba4fe19377fc77210dc214bcf188
Jeremy Alen <alen_jeremy@hotmail.com>
2022-04-01 13:16:17 +0200

    S1-getAllBooks- Logger

--- digibooky/src/main/java/com/bookworms/digibooky/book/api/BookController.java
+++ digibooky/src/main/java/com/bookworms/digibooky/book/api/BookController.java
@@ -2,8 +2,10 @@ package com.bookworms.digibooky.book.api;
 
 import com.bookworms.digibooky.book.api.dto.BookDto;
 import com.bookworms.digibooky.book.service.BookService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 
@@ -18,6 +20,7 @@ public class BookController {
     }
 
     @GetMapping(produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
     public Collection<BookDto> getAll() {
         return bookService.getAll();
     }
--- digibooky/src/main/java/com/bookworms/digibooky/book/service/BookService.java
+++ digibooky/src/main/java/com/bookworms/digibooky/book/service/BookService.java
@@ -2,12 +2,15 @@ package com.bookworms.digibooky.book.service;
 
 import com.bookworms.digibooky.book.api.dto.BookDto;
 import com.bookworms.digibooky.book.domain.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BookService {
+    private Logger serviceLogger = LoggerFactory.getLogger(BookService.class);
 
     private final BookMapper bookMapper;
     private final BookRepository bookRepository;
@@ -18,6 +21,7 @@ public class BookService {
     }
 
     public List<BookDto> getAll() {
+        serviceLogger.info("Showing all books to user.");
         return bookMapper.toDto(bookRepository.getAll());
     }
 }
