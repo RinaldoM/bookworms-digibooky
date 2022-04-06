da035c63d6d13b5629a4af29e4b14314fff2a0df
Jeremy Alen <alen_jeremy@hotmail.com>
2022-04-06 10:26:16 +0200

    SExtra Security - Rental security applied

--- digibooky/src/main/java/com/bookworms/digibooky/rental/api/RentalController.java
+++ digibooky/src/main/java/com/bookworms/digibooky/rental/api/RentalController.java
@@ -4,15 +4,19 @@ import com.bookworms.digibooky.rental.api.dto.CreateRentalDto;
 import com.bookworms.digibooky.rental.api.dto.RentalDto;
 import com.bookworms.digibooky.rental.domain.Rental;
 import com.bookworms.digibooky.rental.service.RentalService;
+import com.bookworms.digibooky.security.SecurityService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static com.bookworms.digibooky.security.Feature.*;
+
 @RestController
 @RequestMapping("rentals")
 public class RentalController {
     private final RentalService rentalService;
+    private SecurityService securityService;
 
     public RentalController(RentalService rentalService) {
         this.rentalService = rentalService;
@@ -31,14 +35,16 @@ public class RentalController {
 
     @GetMapping(path = "/{memberId}", produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
-    public List<RentalDto> getRentalsOfMember(@PathVariable String memberId) {
+    public List<RentalDto> getRentalsOfMember(@RequestHeader String authorizationId, @PathVariable String memberId) {
+        securityService.validateAuthorization(authorizationId, VIEW_LENT_BOOK_BY_MEMBER);
         return rentalService.getRentalsOfMember(memberId);
     }
 
 
     @GetMapping(path = "/overdue", produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
-    public List<RentalDto> getOverdueRentals() {
+    public List<RentalDto> getOverdueRentals(@RequestHeader String authorizationId) {
+        securityService.validateAuthorization(authorizationId, VIEW_OVERDUE_LENT_BOOK);
         return rentalService.getOverdueRentals();
     }
 }
